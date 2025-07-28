52aa6b2ebf49306a42f80554a2406ca9bc2cd1a5
Victor Ozoterio <victorozoterio@gmail.com>
2025-07-28 18:22:07 -0300

    feat: cria componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from './Avatar';
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -10,8 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
+        <Avatar
           src="https://github.com/victorozoterio.png"
         />
         <strong>Victor Ozoterio</strong>
